5a0895bd398430393cd127c4a8cdffacfe92698a
Vividh Mariya <git@vmariya.com>
2022-11-27 04:40:28 +0530

    final fixes

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.purple,
         ),
+        debugShowCheckedModeBanner: false,
         home: Navbar());
   }
 }
